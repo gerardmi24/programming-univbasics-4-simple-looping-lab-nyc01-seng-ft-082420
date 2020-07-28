0b5e8602994debc78dc26c17cb4984e9c75c23a6
Gerard Mistretta <gerardmistretta@yahoo.com>
2020-07-28 23:29:18 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -1,9 +1,10 @@
 # Write your methods here
 def loop_message_five_times (question)
 count = 0
-while count < 5 do
+while count <= 4 do
 puts How am I doing?
 count += 1
+end
 end
 
 def loop_message_n_times
